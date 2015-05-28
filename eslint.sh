ff8a39488d200cae38834f7aaab7dec455ce4a1e
Kanstantsin Kamkou <kkamkou@gmail.com>
2015-05-28 17:26:28 +0200

    draft

--- eslint.sh
+++ eslint.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-docker run --rm -i -v "${PWD}:/opt" eslint "$@"
+DIR="${PWD}"
+for (( i=1; i<=$#; i++ )); do
+  case "${!i}" in
+    --stdin-filename)
+      DIR="${DIR/$(dirname ${@:$((i+1))})}"
+    ;;
+    *);;
+  esac
+done
+
+docker run --rm -i -v "${DIR%/}:/opt" eslint "$@"
