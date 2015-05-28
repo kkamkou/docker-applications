badd3ebe8722dfe3e7c10c2d2557ded10873a6e5
Kanstantsin Kamkou <kkamkou@gmail.com>
2015-05-29 00:51:37 +0200

    typo

--- eslint.sh
+++ eslint.sh
@@ -3,10 +3,10 @@
 IFS=";"
 
 CMD="docker run --rm -i"
-for p in `echo "${SUBLIME_ESLINT_FOLDERS:=$PWD}"`; do
+for p in `echo "${SUBLIME_ESLINT_FOLDERS:-$PWD}"`; do
   DIRNAME="/opt/$(basename ${p})"
   CMD="${CMD} -v '${p}:${DIRNAME}'"
 done
 
-CMD="${CMD} -w '${DIRNAME}/`dirname ${SUBLIME_ESLINT_FILE}`' eslint ${@}"
+CMD="${CMD} -w '${DIRNAME}/`dirname ${SUBLIME_ESLINT_FILE:-.}`' eslint ${@}"
 eval $CMD
